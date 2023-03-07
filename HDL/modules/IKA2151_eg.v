c349b206c94f46e662ca010f9fc9eb8ab71c56f5
Raki <mikoto0931@gmail.com>
2023-03-07 15:43:38 +0900

    Update 20230307

--- HDL/modules/IKA2151_eg.v
+++ HDL/modules/IKA2151_eg.v
@@ -27,6 +27,9 @@ module IKA2151_eg
     input   wire    [7:0]   i_LFA, //amplitude modulation from LFO
     input   wire    [7:0]   i_TEST, //test register
 
+    //input data
+    input   wire    [4:0]   i_EG_PDELTA_SHIFT_AMOUNT,
+
     //output data
     output  wire    [9:0]   o_OP_ENV_LEVEL, //envelope level
     output  wire            o_NOISE_ENV_LEVEL, //envelope level(for noise module)
@@ -159,6 +162,14 @@ end
 //////  Previous KON shift register
 ////
 
+/*
+                                             previous KON data
+                                     |----------(32 stages)---------|
+    i_KON(cyc5) -> (cyc6 - cyc9) -+> (cyc10 - cyc37) -> (cyc6 - cyc9) -> -o|¯¯¯¯\
+                                  |                                        | AND )--- positive edge detector
+                                  +------------------------------------> --|____/
+*/
+
 //These shift registers holds KON values from previous 32 cycles
 reg     [3:0]   cyc6r_cyc9r_kon_current_dlyline; //outer process delay compensation(4 cycles)
 reg     [27:0]  cyc10r_cyc37r_kon_previous; //previous KON values
@@ -186,11 +197,19 @@ end
 //////  Envelope state machine
 ////
 
+/*
+    Envelope state machine holds the states of 32 operators
+
+                  (state update)
+                        |
+                        V
+    (cyc6 - cyc9) -> (cyc10 - cyc37) (loop to cyc 6, total 32 stages) 
+*/
+
 localparam ATTACK = 2'd0;
 localparam FIRST_DECAY = 2'd1;
 localparam SECOND_DECAY = 2'd2;
 localparam RELEASE = 2'd3;
-
 
 //
 //  combinational part
@@ -200,7 +219,6 @@ localparam RELEASE = 2'd3;
 wire            cyc10c_first_decay_end;
 wire            cyc10c_env_level_max;
 wire            cyc10c_env_level_min;
-wire    [1:0]   cyc10c_envstate_previous = cyc6r_cyc9r_envstate_previous[3];
 
 
 //
@@ -212,11 +230,13 @@ reg     [1:0]   cyc6r_cyc9r_envstate_previous[0:3]; //4 stages
 reg     [1:0]   cyc10r_envstate_current; //1 stage
 reg     [1:0]   cyc11r_cyc37r_envstate_previous[0:26]; //27 stages
 
+wire    [1:0]   cyc10c_envstate_previous = cyc6r_cyc9r_envstate_previous[3];
+
 //sr4
 always @(posedge i_EMUCLK) begin
     if(!phi1ncen_n) begin
         //if kon detected, make previous envstate ATTACK
-        cyc6r_cyc9r_envstate_previous[0] <= (~cyc11r_cyc37r_kon_previous[27] & i_KON) ? ATTACK : cyc11r_cyc37r_envstate_previous[26];
+        cyc6r_cyc9r_envstate_previous[0] <= (~cyc11r_cyc37r_kon_previous[26] & i_KON) ? ATTACK : cyc11r_cyc37r_envstate_previous[26];
         cyc6r_cyc9r_envstate_previous[1] <= cyc6r_cyc9r_envstate_previous[0];
         cyc6r_cyc9r_envstate_previous[2] <= cyc6r_cyc9r_envstate_previous[1];
         cyc6r_cyc9r_envstate_previous[3] <= cyc6r_cyc9r_envstate_previous[2];
@@ -342,13 +362,187 @@ always @(posedge i_EMUCLK) begin
     if(!phi1ncen_n) begin
         cyc8r_egparam <= cyc8c_egparam;
         cyc8r_egparam_zero <= cyc8c_egparam == 5'd0;
+
+        case(i_KS)
+            2'd0: cyc8r_keyscale <= (cyc8c_egparam == 5'd0) ? 5'd0 : {3'b000, i_EG_PDELTA_SHIFT_AMOUNT[4:3]};
+            2'd1: cyc8r_keyscale <= {2'b00, i_EG_PDELTA_SHIFT_AMOUNT[4:2]};
+            2'd2: cyc8r_keyscale <= {1'b0, i_EG_PDELTA_SHIFT_AMOUNT[4:1]};
+            2'd3: cyc8r_keyscale <= i_EG_PDELTA_SHIFT_AMOUNT;
+        endcase
     end
 end
 
 
 
+///////////////////////////////////////////////////////////
+//////  Data pipeline Cycle 9: apply KS
+////
+
+//
+//  combinational part
+//
+
+wire    [6:0]   cyc9c_egparam_scaled_adder = {cyc8r_egparam, 1'b0} + {1'b0, cyc8r_keyscale};
 
 
+//
+//  register part
+//
+
+reg             cyc9r_egparam_zero;
+reg     [5:0]   cyc9r_egparam_scaled;
+
+reg             cyc9r_third_sample;
+reg     [1:0]   cyc9r_envcntr;
+reg     [3:0]   cyc9r_attenrate;
+
+always @(posedge i_EMUCLK) begin
+    if(!phi1ncen_n) begin
+        cyc9r_egparam_zero <= cyc8r_egparam_zero;
+        cyc9r_egparam_scaled <= cyc9c_egparam_scaled_adder[6] ? 6'd0 : cyc9c_egparam_scaled_adder[5:0]; //saturation
+
+        cyc9r_third_sample <= third_sample;
+        cyc9r_envcntr <= envcntr;
+        cyc9r_attenrate <= attenrate;
+    end
+end
+
+
+
+///////////////////////////////////////////////////////////
+//////  Data pipeline Cycle 10: make envelope weight
+////
+
+/*
+    See "Attenuation rate generator" section. Attenrate value is determined
+    from the counter value like below:
+
+    timecntr = X_0000_00000_00000 = 0
+    timecntr = X_1000_00000_00000 = 14
+    timecntr = X_X100_00000_00000 = 13
+    timecntr = X_XX10_00000_00000 = 12
+    ...
+    timecntr = X_XXXX_XXXXX_XXX10 = 2
+    timecntr = X_XXXX_XXXXX_XXXX1 = 1
+
+    An attenuation rate of 1 will occur most often, 14 or 0 will occur 
+    least often. Attenuation rate * 4 (2-bit left shift) is added to 
+    the "egparam_scaled" to get the final value.
+
+    Therefore, the quadrupled values that should be added to 
+    "egparam_scaled" are:
+
+    least often <----                      ----> most often
+    0, 56, 52, 48, 44, 40, 36, 32, 28, 24, 20, 16, 12, 8, 4
+
+
+    If the "egparam_scaled" is NOT 11XXXX, there are three conditions 
+    that can change the envelope value:
+
+    1. egparam_scaled      != from 6'd48 to 6'd63
+       egparam_scaled      != 6'd0
+       egparam_rateapplied == from 6'd48 to 6'd51
+
+    2. egparam_scaled      != from 6'd48 to 6'd63
+       egparam_rateapplied == 6'd54 or 6'd55
+
+    3. egparam_scaled      != from 6'd48 to 6'd63
+       egparam_rateapplied == 6'd57 or 6'd59
+
+    These three conditions can be compressed like this:
+        egparam_scaled      != from 6'd48 to 6'd63
+        egparam_scaled      != 6'd0
+        egparam_rateapplied == 48, 49, 50, 51, 54, 55, 57, 59
+
+
+    Therefore, if "egparam scaled" is 1, this value can change the envelope
+    level when the rate is "48"
+    if "egparam_scaled" is 2, this value can change the envelope level when
+    the rate is "48" or "52"
+    if 3, the value-changable rate is "48" or "52" or "59"
+    if 4, the value-changable rate is "44"
+
+    The rate "44" appears more often than the sum of the frequency of
+    occurrence of "48", "52", "59". So, egparam_scaled = 4 can change the
+    envelope level often, than 1, 2, 3. If the envelope level changes frequently,
+    the difference between the envelope level of the current sample and the next 
+    sample becomes larger.
+*/
+
+//
+//  combinational part
+//
+
+reg             cyc10c_envdeltaweight_intensity; //0 = weak, 1 = strong
+always @(*) begin
+    case({cyc9r_egparam_scaled[1:0], cyc9r_envcntr})
+        4'b00_00: cyc10c_envdeltaweight_intensity <= 1'b0;
+        4'b00_01: cyc10c_envdeltaweight_intensity <= 1'b0;
+        4'b00_10: cyc10c_envdeltaweight_intensity <= 1'b0;
+        4'b00_11: cyc10c_envdeltaweight_intensity <= 1'b0;
+
+        4'b01_00: cyc10c_envdeltaweight_intensity <= 1'b1;
+        4'b01_01: cyc10c_envdeltaweight_intensity <= 1'b0;
+        4'b01_10: cyc10c_envdeltaweight_intensity <= 1'b0;
+        4'b01_11: cyc10c_envdeltaweight_intensity <= 1'b0;
+
+        4'b10_00: cyc10c_envdeltaweight_intensity <= 1'b1;
+        4'b10_01: cyc10c_envdeltaweight_intensity <= 1'b0;
+        4'b10_10: cyc10c_envdeltaweight_intensity <= 1'b1;
+        4'b10_11: cyc10c_envdeltaweight_intensity <= 1'b0;
+
+        4'b11_00: cyc10c_envdeltaweight_intensity <= 1'b1;
+        4'b11_01: cyc10c_envdeltaweight_intensity <= 1'b1;
+        4'b11_10: cyc10c_envdeltaweight_intensity <= 1'b1;
+        4'b11_11: cyc10c_envdeltaweight_intensity <= 1'b0;
+    endcase
+end
+
+wire    [5:0]   cyc10c_egparam_rateapplied = cyc9r_egparam_scaled + {attenrate, 2'b00}; //discard carry
+
+
+//
+//  register part
+//
+
+reg     [3:0]   cyc10r_envdeltaweight; //lv4, lv3, lv2, lv1
+always @(posedge i_EMUCLK) begin
+    if(!phi1ncen_n) begin
+        if(cyc9r_third_sample) begin
+            if     (cyc9r_egparam_scaled[5:2] == 4'b1111) cyc10r_envdeltaweight <= cyc10c_envdeltaweight_intensity ? 4'b1000 : 4'b1000;
+            else if(cyc9r_egparam_scaled[5:2] == 4'b1110) cyc10r_envdeltaweight <= cyc10c_envdeltaweight_intensity ? 4'b1000 : 4'b0100;
+            else if(cyc9r_egparam_scaled[5:2] == 4'b1101) cyc10r_envdeltaweight <= cyc10c_envdeltaweight_intensity ? 4'b0100 : 4'b0010;
+            else if(cyc9r_egparam_scaled[5:2] == 4'b1100) cyc10r_envdeltaweight <= cyc10c_envdeltaweight_intensity ? 4'b0010 : 4'b0001;
+            else begin
+                if(cyc9r_egparam_zero) begin
+                    cyc10r_envdeltaweight <= 4'b0000;
+                end
+                else begin
+                    if(cyc9r_egparam_scaled != 6'd0 & 
+                       |{cyc10c_egparam_rateapplied == 6'd59, cyc10c_egparam_rateapplied == 6'd57,
+                         cyc10c_egparam_rateapplied == 6'd55, cyc10c_egparam_rateapplied == 6'd54,
+                         cyc10c_egparam_rateapplied == 6'd51, cyc10c_egparam_rateapplied == 6'd50,
+                         cyc10c_egparam_rateapplied == 6'd49, cyc10c_egparam_rateapplied == 6'd48}) begin
+                        
+                        cyc10r_envdeltaweight <= 4'b0001;
+                    end
+                    else begin
+                        cyc10r_envdeltaweight <= 4'b0000;
+                    end
+                end
+            end
+        end
+        else begin
+            cyc10r_envdeltaweight <= 4'b0000;
+        end
+    end
+end
+
+
+
+///////////////////////////////////////////////////////////
+//////  Data pipeline Cycle 9: apply KS
+////
 
 
 
