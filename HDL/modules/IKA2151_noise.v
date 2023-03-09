6f01f0bf6db231fbc1620052bd20ba012561094d
Raki <mikoto0931@gmail.com>
2023-03-09 16:31:27 +0900

    Add noise

---
+++ HDL/modules/IKA2151_noise.v
@@ -0,0 +1,40 @@
+module IKA2151_noise
+(
+    //master clock
+    input   wire            i_EMUCLK, //emulator master clock
+
+    //core internal reset
+    input   wire            i_MRST_n,
+
+    //internal clock
+    input   wire            i_phi1_PCEN_n, //positive edge clock enable for emulation
+    input   wire            i_phi1_NCEN_n, //engative edge clock enable for emulation
+
+    //timings
+    input   wire            i_CYCLE_03,
+    input   wire            i_CYCLE_12,
+    input   wire            i_CYCLE_14_30,
+
+    //register data
+    input   wire    [4:0]   i_NFRQ,
+
+    //output data
+    output  wire    [13:0]  o_ACC_NOISE
+);
+
+
+
+///////////////////////////////////////////////////////////
+//////  Clock and reset
+////
+
+wire            phi1pcen_n = i_phi1_PCEN_n;
+wire            phi1ncen_n = i_phi1_NCEN_n;
+wire            mrst_n = i_MRST_n;
+
+
+
+
+
+
+endmodule
